3a59dc46ff8272f0cc42498c0d9c1dc01197ec8e
Umar Amirshoev <amirshoevumarrrrr.1@gmail.com>
2026-03-13 10:59:33 +0500

    delete comment

--- main.go
+++ main.go
@@ -21,37 +21,6 @@ func main() {
 
 // package main
 
-// import "fmt"
-
-// func sumPart(arr []int, ch chan int) {
-// 	sum := 0
-// 	for _, num := range arr {
-// 		sum += num
-// 	}
-// 	ch <- sum
-// }
-
-// func main() {
-// 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
-// 	numGoroutines := 3
-// 	ch := make(chan int, numGoroutines)
-
-// 	partize := len(arr) / numGoroutines
-
-// 	for i := 0; i < numGoroutines; i++ {
-// 		start := i * partize
-// 		end := start + partize
-// 		go sumPart(arr[start:end], ch)
-// 	}
-// 	totalsum := 0
-// 	for i := 0; i < numGoroutines; i++ {
-// 		totalsum += <-ch
-// 	}
-// 	fmt.Println("Total sum: ", totalsum)
-// }
-
-// package main
-
 // import (
 // 	"fmt"
 // 	"net/http"
